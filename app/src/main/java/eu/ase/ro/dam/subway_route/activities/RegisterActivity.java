dd6bbfdaaf8a1e2fdf201b3a1567ddf7a8799cfb
Alexandra IV Dumitru <alexandra_florentina22@yahoo.com>
2019-12-26 17:12:04 +0200

    Autentificare Blana
    la la la

--- app/src/main/java/eu/ase/ro/dam/subway_route/activities/RegisterActivity.java
+++ app/src/main/java/eu/ase/ro/dam/subway_route/activities/RegisterActivity.java
@@ -1,14 +1,23 @@
 package eu.ase.ro.dam.subway_route.activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthUserCollisionException;
+import com.google.firebase.auth.FirebaseUser;
 
 import eu.ase.ro.dam.subway_route.R;
 
@@ -18,6 +27,7 @@ public class RegisterActivity extends AppCompatActivity {
     TextInputEditText et_confirm_password;
     Button button_register;
     Intent intent;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,18 +42,55 @@ public class RegisterActivity extends AppCompatActivity {
         et_password = findViewById(R.id.register_et_password);
         et_confirm_password = findViewById(R.id.register_et_repeat_password);
         button_register = findViewById(R.id.register_button);
+        mAuth = FirebaseAuth.getInstance();
 
         button_register.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if(infoValidation()) {
-                    Toast.makeText(getApplicationContext(), R.string.info_creare_user, Toast.LENGTH_LONG).show();
-                    intent = new Intent(RegisterActivity.this, LoginActivity.class);
-                    startActivity(intent);
-                    finish();
+                    auth();
                 }
             }
         });
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser != null){
+            Toast.makeText(getApplicationContext(), R.string.errOnStartSignIn, Toast.LENGTH_LONG).show();
+        }
+    }
+
+    private void auth(){
+        final String email = et_email.getText().toString().trim();
+        final String password = et_password.getText().toString().trim();
+
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // autentificare cu succes
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            Toast.makeText(getApplicationContext(), R.string.info_creare_user, Toast.LENGTH_LONG).show();
+                            intent = new Intent(RegisterActivity.this, LoginActivity.class);
+                            startActivity(intent);
+                            finish();
+                        } else {
+                            // autentificare nereusita
+                            if(task.getException() instanceof FirebaseAuthUserCollisionException){
+                                Toast.makeText(getApplicationContext(), R.string.errSameUser, Toast.LENGTH_SHORT).show();
+                            }
+                            else{
+                                Toast.makeText(getApplicationContext(), R.string.errAuth, Toast.LENGTH_SHORT).show();
+                            }
+                            //Toast.makeText(getApplicationContext(), task.getException().getMessage(), Toast.LENGTH_LONG).show();
+                        }
+
+                    }
+                });
     }
 
     private boolean infoValidation(){
@@ -51,12 +98,20 @@ public class RegisterActivity extends AppCompatActivity {
             Toast.makeText(getApplicationContext(), R.string.err_mail, Toast.LENGTH_LONG).show();
             return false;
         }
-        if(et_password.getText() == null || et_password.getText().toString().trim().isEmpty()){
+        if(!Patterns.EMAIL_ADDRESS.matcher(et_email.getText().toString().trim()).matches()){
+            Toast.makeText(getApplicationContext(), R.string.errValidEmail, Toast.LENGTH_LONG).show();
+            return false;
+        }
+        if(et_password.getText() == null || et_password.getText().toString().trim().isEmpty() || et_password.getText().toString().trim().length()<7){
             Toast.makeText(getApplicationContext(), R.string.err_set_pass, Toast.LENGTH_LONG).show();
             return false;
         }
         if(et_confirm_password.getText() == null || et_confirm_password.getText().toString().trim().isEmpty()) {
             Toast.makeText(getApplicationContext(), R.string.err_confirm_pass, Toast.LENGTH_LONG).show();
+            return false;
+        }
+        if(!et_confirm_password.getText().toString().trim().matches(et_password.getText().toString().trim())){
+            Toast.makeText(getApplicationContext(), R.string.errSamePass, Toast.LENGTH_LONG).show();
             return false;
         }
         return true;
